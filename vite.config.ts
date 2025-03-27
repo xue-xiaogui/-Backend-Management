0666e20cee97a258b0049ab5a97f8ea6d1fdc2a8
xue-xiaogui <613238634@qq.com>
2025-03-28 06:20:31 +0800

    feat:修改提交1

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
